fa0681483b0d779b2b1ccf97eacc494647408964
blessingarizona <blessingarinze9@gmail.com>
2023-05-10 18:11:33 +0100

    commit message

--- task/main.py
+++ task/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, Response, Depends, status, HTTPException
 from typing import Optional 
 import schemas, models
 from typing import List
@@ -17,7 +17,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/todo/")
+@app.post("/todo/", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Todo, db: Session = Depends(get_db)):
     new_todo = models.Todo(title=request.title, body=request.body)
     db.add(new_todo)
@@ -29,3 +29,12 @@ def create(request: schemas.Todo, db: Session = Depends(get_db)):
 def all(db: Session = Depends(get_db)):
     todos = db.query(models.Todo).all()
     return todos
+
+@app.get("/todo/{id}", status_code=200)
+def show(id, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == id).first()
+    if not todo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Todo with the id {id} is not available")
+        
+    return todo
+    
